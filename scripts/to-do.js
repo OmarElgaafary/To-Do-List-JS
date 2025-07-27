93f630639bee4054ed57aadd330b8b8df88c9872
Omar Adly <omarelgaafaryy@gmail.com>
2025-07-28 01:34:08 +0300

    Added JS interactivity.

---
+++ scripts/to-do.js
@@ -0,0 +1,64 @@
+const toDoDeleteButton = document.getElementsByClassName('to-do-delete');
+const toDoItemContainer = document.querySelector('.to-do-items-div');
+const toDoSearchBar = document.querySelector('.to-do-search-bar');
+const toDoAddButton = document.querySelector('.to-do-add-button');
+
+let toDos = [{
+    text: 'mytodo',
+    id: '123'
+}];
+
+
+
+function generateToDoId(length) {
+    const availableChars = '1234567890abcdefhijklmnopqrstuv!@#$%^&*';
+    let generatedToDoId = '';
+    for (var i = 0; i < length; ++i) {
+        generatedToDoId += availableChars.charAt(Math.floor(Math.random() * availableChars.length));
+    }
+    return generatedToDoId;
+}
+
+function addToDo() {
+    toDoAddButton.addEventListener('click', () => {
+        const toDoSearchInput = toDoSearchBar.value;
+        const toDoId = generateToDoId(12);
+        if (!toDoSearchBar)
+            return;
+        else {
+            toDos.push({
+                text: toDoSearchInput,
+                id: toDoId
+            })
+        }
+        renderToDos();
+    });
+}
+
+function renderToDos() {
+    toDoItemContainer.innerHTML = '';
+    toDos.forEach((toDo) => {
+        toDoItemContainer.innerHTML += `<div class="to-do-item">
+        <input class="to-do-checkbox" type="checkbox">
+        <p class="to-do-item-p">${toDo.text}</p>
+        <button class="to-do-delete" data-id="${toDo.id}"><span class="material-symbols-outlined">
+            close
+          </span></button>
+      </div>`;
+    });
+    deleteToDo();
+}
+
+function deleteToDo() {
+    for (let i = 0; i < toDoDeleteButton.length; i++) {
+        toDoDeleteButton[i].addEventListener('click', () => {
+            console.log('delete fired');
+            const toDoDeleteID = toDoDeleteButton[i].dataset.id;
+            toDos = toDos.filter(toDo => toDo.id !== toDoDeleteID);
+            renderToDos();
+        })
+    }
+}
+renderToDos();
+generateToDoId();
+addToDo();
